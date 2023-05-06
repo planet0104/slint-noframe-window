843791ed75c620794adf8abced38822b6f06772c
JiaYe <planet2@qq.com>
2023-05-06 14:55:15 +0800

    1.0

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+use slint::LogicalPosition;
+
+slint::include_modules!();
+
+fn main() -> Result<()> {
+    let main = Main::new()?;
+
+    let hanel1 = main.as_weak();
+    main.on_close_window(move ||{
+        hanel1.upgrade().unwrap().hide().unwrap();
+    });
+
+    let handle2 = main.as_weak();
+    main.on_move_window(move |offset_x, offset_y|{
+        let main = handle2.upgrade().unwrap();
+        let logical_pos = main.window().position().to_logical(main.window().scale_factor());
+        main.window().set_position(LogicalPosition::new(logical_pos.x + offset_x, logical_pos.y + offset_y));
+    });
+
+    main.run()?;
+    Ok(())
+}
